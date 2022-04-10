f573bcd4ff7c7bf12d08bab539c385f4ffffed68
dalton05 <dalton05@gmail.com>
2022-04-10 15:48:46 -0600

    mostly works. no css

--- js/main.js
+++ js/main.js
@@ -24,26 +24,35 @@ function pickDrink(obj) {
 
 
 function updateDom(obj) {
+    
     const drink = obj.drinks[0]
+    console.log(drink)
+    const ingredientsArr = [drink.strIngredient1,
+                            drink.strIngredient2, 
+                            drink.strIngredient3, 
+                            drink.strIngredient4,
+                            drink.strIngredient5, 
+                            drink.strIngredient6, 
+                            drink.strIngredient7, 
+                            drink.strIngredient8, 
+                            drink.strIngredient9, 
+                            drink.strIngredient10, 
+                            drink.strIngredient11, 
+                            drink.strIngredient12, 
+                            drink.strIngredient13, 
+                            drink.strIngredient14, 
+                            drink.strIngredient15]
+
     document.querySelector('#drinkTitle').innerText = drink.strDrink
     document.querySelector('img').src = drink.strDrinkThumb
     document.querySelector('#categoryAndGlass').innerText = `${drink.strCategory} | ${drink.strGlass}`
     
     
-    document.querySelector('#ingredient1').innerText = drink.strIngredient1
-    document.querySelector('#ingredient2').innerText = drink.strIngredient2
-    document.querySelector('#ingredient3').innerText = drink.strIngredient3
-    document.querySelector('#ingredient4').innerText = drink.strIngredient4
-    document.querySelector('#ingredient5').innerText = drink.strIngredient5
-    document.querySelector('#ingredient6').innerText = drink.strIngredient6
-    document.querySelector('#ingredient7').innerText = drink.strIngredient7
-    document.querySelector('#ingredient8').innerText = drink.strIngredient8
-    document.querySelector('#ingredient9').innerText = drink.strIngredient9
-    document.querySelector('#ingredient10').innerText = drink.strIngredient10
-    document.querySelector('#ingredient11').innerText = drink.strIngredient11
-    document.querySelector('#ingredient12').innerText = drink.strIngredient12
-    document.querySelector('#ingredient13').innerText = drink.strIngredient13
-    document.querySelector('#ingredient14').innerText = drink.strIngredient14
-    document.querySelector('#ingredient15').innerText = drink.strIngredient15
+    for (let i = 0; i <= ingredientsArr.length; i++) {
+        document.querySelector(`#ingredient${i+1}`).innerText = ''
+        if (ingredientsArr[i]) {
+            document.querySelector(`#ingredient${i+1}`).innerText = `${drink[`strMeasure${i+1}`]} ${ingredientsArr[i]}`
+        }
+    }
 
 }
